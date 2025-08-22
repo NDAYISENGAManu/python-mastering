13ccc4810646cedf4901bbe6e1173ba2e5922de8
endayisenga <endayisenga@bk.rw>
2025-08-22 10:59:51 +0200

    feat: deep dive in python data structures

---
+++ fs.py
@@ -0,0 +1,9 @@
+a = 11
+b = 22
+print(f"the total is {a+b}")
+# String formatting examples
+
+name = "John"
+print("hello", name)
+print("hello "+ name)
+print(f"hello {name}")  # f-string formatting
